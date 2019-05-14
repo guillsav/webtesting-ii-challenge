7f2f72647d9fdff65cfda4f74c6569129ca37643
guillsav <guillsav@gmail.com>
2019-05-14 15:00:48 -0400

    Initial commit

---
+++ metlife-stadium/src/App.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {render} from 'react-testing-library';
+import 'react-testing-library/cleanup-after-each';
+import App from './App';
+
+describe('<App />', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
---
+++ metlife-stadium/src/components/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Dashboard = props => {
+  return (
+    <div className="Dashboard">
+      <button>Strike</button>
+      <button>Ball</button>
+      <button>Foul</button>
+      <button>Hit</button>
+    </div>
+  );
+};
+
+export default Dashboard;
